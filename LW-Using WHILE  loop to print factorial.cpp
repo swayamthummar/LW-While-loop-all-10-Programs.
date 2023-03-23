7b85b4f50f895a9ebb7b039c4a40458a01484884
swayam thummar <127782240+swayamthummar@users.noreply.github.com>
2023-03-23 20:05:12 +0530

    Add files via upload

---
+++ LW-Using WHILE  loop to print factorial.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int f=1,n;
+	 printf("Enter a Number->");
+	 scanf("%d",&n);
+	 
+	 while (n!=0)
+	 {c
+	 	f= f*n;
+	 	n--;
+	 }
+	 printf("Factorial is -> %d",f);
+}
