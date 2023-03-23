8f466d05a59164fec763e46599fe604653af4f27
swayam thummar <127782240+swayamthummar@users.noreply.github.com>
2023-03-23 17:08:36 +0530

    Add files via upload

---
+++ LW-Using WHILE loop for odd N.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main() 
+{
+    int n,a=0;
+
+    printf("Enter a number-> ");
+    scanf("%d",&n);
+
+    while(a<=n)
+	{
+        if(a%2==0) 
+		{
+            printf("%d\n",a);
+        }
+        a++;
+    }
+}
